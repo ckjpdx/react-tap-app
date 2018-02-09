2cb5c1f89adb1a5a4555de2d299845ae41cc75fb
Chris K Johnson <cknightjohnson@gmail.com>
2018-02-09 10:34:30 -0800

    add Error404

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'; // always import core React library
 import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
 import Welcome from './Welcome';
 import KegList from './KegList';
 
 function App(){
   return (
     <div>
-      <h1>App Works</h1>
+      <h1>React Tap App</h1>
       <Switch>
         <Route exact path='/' component={Welcome} />
         <Route path='/keglist' component={KegList} />
+        <Route component={Error404}/>
       </Switch>
     </div>
   );
---
+++ src/components/Error404.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; // always import core React library
+import { Link } from 'react-router-dom';
+
+function Error404(){
+  return (
+    <div>
+      <h1>Whoops!</h1>
+      <h2>Looks like you're lost. You should head back <Link to="/">Home</Link> where it's safe!</h2>
+    </div>
+  );
+}
+
+export default Error404;
