444eaa2fa069e8f945f0ce6c2b8ecf04baf3b094
Chris K Johnson <cknightjohnson@gmail.com>
2018-02-09 10:18:10 -0800

    add routes, KegList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'; // always import core React library
 import { Switch, Route } from 'react-router-dom';
 import Welcome from './Welcome';
+import KegList from './KegList';
 
 function App(){
   return (
     <div>
       <h1>App Works</h1>
-      <Welcome/>
       <Switch>
+        <Route exact path='/' component={Welcome} />
+        <Route path='/keglist' component={KegList} />
       </Switch>
     </div>
   );
