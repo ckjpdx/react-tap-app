2487f5c7f73af48c8d01997a3ff565e4ae620eb9
Chris K Johnson <cknightjohnson@gmail.com>
2018-02-09 15:11:12 -0800

    add styles to navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,24 +6,35 @@ import KegList from './KegList';
 import NavHeader from './NavHeader';
 
 function App(){
+  let topFlexBar = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: 20
+  };
   return (
-    <div>
+    <section>
       <style jsx global>{`
         * {
           text-align: center;
           margin: 0;
           padding: 0;
           box-sizing: border-box;
+          list-style: none;
+        }
+        img {
+          width: 100%
         }
       `}</style>
-      <h1>React Tap App</h1>
-      <NavHeader/>
+    <div style={topFlexBar}>
+        <h1>React Tap App</h1>
+        <NavHeader/>
+      </div>
       <Switch>
         <Route exact path='/' component={Welcome} />
         <Route path='/keglist' component={KegList} />
         <Route component={Error404}/>
       </Switch>
-    </div>
+    </section>
   );
 }
 
--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function NavHeader(){
+  let linkStyle = {
+    textDecoration: 'none',
+    color: 'black',
+    padding: 10,
+    margin: 5,
+    background: 'gray',
+    borderRadius: 5
+  }
   return (
-    <div>
-      <p>Navigation</p>
-      <nav>
-        <Link to='/'>Welcome</Link>
-        <Link to='/keglist'>Keg List</Link>
-      </nav>
+    <div id='flex-container'>
+      <style jsx>{`
+          #flex-container {
+            display: flex;
+            justify-content: space-around;
+          }
+          p {
+            font-size: 0.8em;
+            color: orange;
+            text-transform: uppercase;
+            letter-spacing: 5px;
+
+          }
+          `}</style>
+      <div>
+        <p>Navigation</p>
+      </div>
+      <div>
+        <nav>
+          <Link to='/' style={linkStyle}>Welcome</Link>
+          <Link to='/keglist' style={linkStyle}>Keg List</Link>
+        </nav>
+      </div>
     </div>
   );
 }
