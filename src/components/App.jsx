c73135c8fe48b40493f91dd4ae63dc344ecb1306
Chris K Johnson <cknightjohnson@gmail.com>
2018-02-09 10:08:43 -0800

    add subcomponent Welcome

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'; // always import core React library
 import { Switch, Route } from 'react-router-dom';
+import Welcome from './Welcome';
 
 function App(){
   return (
     <div>
       <h1>App Works</h1>
+      <Welcome/>
       <Switch>
       </Switch>
     </div>
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; // always import core React library
+
+function Welcome(){
+  return (
+    <div>
+      <h1>Welcome Works</h1>
+    </div>
+  );
+}
+
+export default Welcome;
