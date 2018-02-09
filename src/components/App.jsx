ee5b227d60b94b24900af3d535021bf939f58f7a
Chris K Johnson <cknightjohnson@gmail.com>
2018-02-09 10:06:57 -0800

    add entry point index jsx, App jsx to test setup

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; // always import core React library
+import { Switch, Route } from 'react-router-dom';
+
+function App(){
+  return (
+    <div>
+      <h1>App Works</h1>
+      <Switch>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
